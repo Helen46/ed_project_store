82b3895a1240e38e1ccd7a55e7d7c92cca9cb2b3
Елена Прилуцкая <a.priluckay87@gmail.com>
2024-09-12 00:06:48 +0300

    add template inc_menu

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,6 +7,6 @@ from catalog.views import product_list, contacts
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", product_list, name="base"),
+    path("", product_list, name="product_list"),
     path("contacts/", contacts, name="contacts")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
