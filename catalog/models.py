d6bbb7558fc25d9bb59a1d103d48c630684d1c25
Елена Прилуцкая <a.priluckay87@gmail.com>
2024-09-11 13:50:36 +0300

    add models Product and Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name="Наименование",
+        help_text="Введите наименование продукта",
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        help_text="Введите описание продукта",
+    )
+    image = models.ImageField(
+        upload_to="products/photo",
+        verbose_name="Фото",
+        help_text="Загрузите фото товара",
+        **NULLABLE,
+    )
+    category = models.ForeignKey(
+        "Category",
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        help_text="Введите категорию продукта",
+        related_name="products",
+        **NULLABLE,
+    )
+    price = models.FloatField(
+        verbose_name="Цена продукта",
+        help_text="Введите цену продукта",
+    )
+    created_at = models.DateTimeField(
+        verbose_name="Дата создания (записи в БД)",
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        verbose_name="Дата последнего изменения (записи в БД)",
+        auto_now=True
+    )
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["category", "name", "price", "created_at", "updated_at"]
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name="Наименование",
+        help_text="Введите наименование категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        help_text="Введите описание категории",
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
--- config/settings.py
+++ config/settings.py
@@ -50,9 +50,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "ed_store",
+        "USER": "postgres",
+        "PASSWORD": "Qwe123Asd$",
+        "HOST": "127.0.0.1",
+        "PORT": "5432"
     }
 }
 
