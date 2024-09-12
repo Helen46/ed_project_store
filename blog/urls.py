0f7856086f67c12cebbad2e059af3b21f4d3ec69
Елена Прилуцкая <a.priluckay87@gmail.com>
2024-09-13 02:00:55 +0300

    add templates delete

--- blog/urls.py
+++ blog/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import BlogCreateView, BlogListView
+from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
     path("create/", BlogCreateView.as_view(), name="blog_create"),
     path("", BlogListView.as_view(), name="blog_list"),
-    # path("view/<int:pk>", BlogUpdateView.as_view(), name="blog_view"),
-    # path("update/<int:pk>", BlogUpdateView.as_view(), name="blog_update"),
-    # path("update/<int:pk>", BlogDeleteView.as_view(), name="blog_delete"),
+    path("view/<int:pk>/", BlogDetailView.as_view(), name="blog_view"),
+    path("update/<int:pk>", BlogUpdateView.as_view(), name="blog_update"),
+    path("delete/<int:pk>", BlogDeleteView.as_view(), name="blog_delete"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
 from blog.models import Blog
 
@@ -7,8 +7,23 @@ from blog.models import Blog
 class BlogCreateView(CreateView):
     model = Blog
     fields = ("title", "content",)
-    success_url = reverse_lazy("catalog:catalog_list")
+    success_url = reverse_lazy("blog:blog_list")
 
 
 class BlogListView(ListView):
     model = Blog
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ("title", "content",)
+    success_url = reverse_lazy("blog:blog_list")
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy("blog:blog_list")
