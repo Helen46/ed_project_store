31f67faa6b05b0aa4d13d58cad5d944eb6f59a62
Елена Прилуцкая <a.priluckay87@gmail.com>
2024-09-12 14:35:12 +0300

    add template product_detail

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import product_list, contacts
+from catalog.views import product_list, contacts, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", product_list, name="product_list"),
+    path("catalog/<int:pk>", product_detail, name="product_detail"),
     path("contacts/", contacts, name="contacts")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product
 
@@ -7,6 +7,12 @@ def product_list(request):
     products = Product.objects.all()
     context = {'catalog': products}
     return render(request, "product_list.html", context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product':product}
+    return render(request, "product_detail.html", context)
 
 
 def contacts(request):
